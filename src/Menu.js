7a197b44fba7b40f59b227b3183d8c8320718a14
cto_varun <cto.varun@gmail.com>
2018-04-12 10:07:39 +0530

    lifecycles done

--- src/Menu.js
+++ src/Menu.js
@@ -2,25 +2,41 @@ import React, {Component} from "react";
 import './Menu.css';
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+        console.log("menu Component's constructor fired");
+    }
+
+    componentWillMount() {
+        console.log("menu Component's component will mount fired");
+    }
+    componentDidMount() {
+        console.log("Component has mounted");
+
+    }
+    componentDidCatch() {
+        console.log("Error in component");
+    }
     render() {
+        console.log("Render function fired");
         return (
             <div className="row">
                 <div className="logo-container"></div>
                 <div className="menu-container">
-                    {
-                        this.props.menus
-                        .map((menu) => {
+                    {this
+                        .props
+                        .menus
+                        .map((menu, i) => {
                             return (
-                                <div className="menu">
+                                <div className="menu" key={i}>
                                     <div>
-                                        { menu.toUpperCase() }
+                                        {menu.toUpperCase()}
                                     </div>
                                 </div>
                             );
                         })
-                    }
+}
 
-                   
                 </div>
             </div>
         );
