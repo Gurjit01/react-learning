7318c1e01bd653f3d5777dd05b1c81a4cbc15ba5
cto_varun <cto.varun@gmail.com>
2018-04-28 09:57:05 +0530

    button added

--- src/MiddleContainer.js
+++ src/MiddleContainer.js
@@ -9,7 +9,8 @@ class MiddleContainer extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			posts: []
+			posts: [],
+			count: 0
 		};
 	}
 	componentWillMount() {
@@ -34,11 +35,16 @@ class MiddleContainer extends Component {
         return (
             <div className="middle-container">
 							<EventHandling clickEvent="testing" keyupEvent="Working"/>
-							{
+								<span>{this.state.count}</span>
+								<button>Count 5 </button>
+								<button>Count 10 </button>
+								<button>Count 15 </button>
+						{
 								this.state.posts.map(
 									(post)=>{
 										return(
 											// <Heading title={post.title} author={post.author} id={post.id}/>
+
 											<Heading {...post}/>
 										)
 									}
